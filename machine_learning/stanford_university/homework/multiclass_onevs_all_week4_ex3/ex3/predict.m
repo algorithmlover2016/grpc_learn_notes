1a0639ed299e6b1eee9da00c1aa466678dbbe7d5
alogirhtmlover <yubo.upt@gmail.com>
2021-06-01 15:00:54 +0800

    add neural network forward propagation function with debug in ex3_nn.m. if you want to debug predictOneVsAll, modify ./vscode/launch.json program and name to ex3.m

--- machine_learning/stanford_university/homework/multiclass_onevs_all_week4_ex3/ex3/predict.m
+++ machine_learning/stanford_university/homework/multiclass_onevs_all_week4_ex3/ex3/predict.m
@@ -21,12 +21,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
+a1 = [ones(m, 1) X];
+z2 = Theta1 * a1';
+a2 = [ones(m, 1) sigmoid(z2')];
+z3 = Theta2 * a2';
+a3 = sigmoid(z3');
+h = a3;
+[maxVal, pos] = max(h, [], 2);
+p = pos;
 
 
 % =========================================================================
