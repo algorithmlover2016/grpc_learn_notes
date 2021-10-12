0f44adc5e543cd1591f93653fdf9db0c68265466
alogirhtmlover <yubo.upt@gmail.com>
2021-10-12 15:10:07 +0800

    add demo from https://jackiexiao.github.io/eat_pytorch_in_20_days/5.%E4%B8%AD%E9%98%B6API/5-4%2CTensorBoard%E5%8F%AF%E8%A7%86%E5%8C%96/

--- python/third-party/tensorboard/kill_tensorboard.sh
+++ python/third-party/tensorboard/kill_tensorboard.sh
@@ -3,3 +3,9 @@ to kill by name:
 taskkill /IM "tensorboard.exe" /F
 to kill by process number:
 taskkill /F /PID proc_num
+
+In Windows powershell type
+to kill by name:
+Stop-Process -Name "ProcessName" -Force
+to kill by process number(pid, proc_num)
+Stop-Process -ID PID -Force
