0e5d5ff7e1b43b14d018bad74ffd13f55149189d
alogirhtmlover <yubo.upt@gmail.com>
2021-11-25 15:57:44 +0800

    add logging configuration

---
+++ python/logging/setting_log.py
@@ -0,0 +1,29 @@
+# reference to https://stackoverflow.com/questions/533048/how-to-log-source-file-name-and-line-number-in-python
+import logging
+import sys
+
+# %(pathname)s Full pathname of the source file where the logging call was issued(if available).
+# %(filename)s Filename portion of pathname.
+# %(module)s Module (name portion of filename).
+# %(funcName)s Name of function containing the logging call.
+# %(lineno)d Source line number where the logging call was issued (if available).
+
+logging.basicConfig(format='%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+logger.debug("This is a debug log")
+logger.info("This is an info log")
+logger.critical("This is critical")
+logger.error("An error occurred")
+
+formatter = logging.Formatter('[%(asctime)s.%(msecs)d] p%(process)s [%(pathname)s:%(lineno)s] %(levelname)-8s - %(message)s','%Y%m%d %H:%M:%S')
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logging.debug("I am sent to standard out.")
+logger.debug("I am sent to standard out.")
