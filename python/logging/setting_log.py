3264fefa473c4674c4e6148c1754f6dd68f73b3e
alogirhtmlover <yubo.upt@gmail.com>
2022-03-04 17:03:24 +0800

    add python logging configuration without useness

--- python/logging/setting_log.py
+++ python/logging/setting_log.py
@@ -1,6 +1,7 @@
 # reference to https://newbedev.com/how-to-log-source-file-name-and-line-number-in-python
 import logging
 import sys
+import os
 
 # reference to https://stackoverflow.com/questions/533048/how-to-log-source-file-name-and-line-number-in-python
 # %(pathname)s Full pathname of the source file where the logging call was issued(if available).
@@ -20,11 +21,34 @@ logger.info("This is an info log")
 logger.critical("This is critical")
 logger.error("An error occurred")
 
-formatter = logging.Formatter('[%(asctime)s.%(msecs)d] p%(process)s [%(pathname)s:%(lineno)s] %(levelname)-8s - %(message)s','%Y%m%d %H:%M:%S')
+formatter = logging.Formatter('[%(asctime)s.%(msecs)d] %(levelname)-8s [%(pathname)s:%(lineno)s] p[%(process)s] - %(message)s','%Y%m%d %H:%M:%S')
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+logging.debug("I am sent to standard out. by logging")
+logger.debug("I am sent to standard out.")
+
+# refer to 
+class PackagePathFilter(logging.Filter):
+    def filter(self, record):
+        pathname = record.pathname
+        record.relativepath = None
+        abs_sys_paths = map(os.path.abspath, sys.path)
+        for path in sorted(abs_sys_paths, key = len, reverse = True): # longer paths first
+            if not path.endswith(os.sep):
+                path += os.sep
+            if pathname.startswith(path):
+                record.relativepath = os.path.relpath(pathname, path)
+                break
+        return True
+
+formatter = logging.Formatter('[%(asctime)s.%(msecs)d] %(levelname)-8s [%(relativepath)s:%(lineno)s] pp[%(process)s] - %(message)s','%Y%m%d %H:%M:%S')
+relativeCH = logging.StreamHandler(sys.stdout)
+relativeCH.setLevel(logging.DEBUG)
+relativeCH.setFormatter(formatter)
+relativeCH.addFilter(PackagePathFilter())
+logger.addHandler(relativeCH)
 logging.debug("I am sent to standard out. by logging")
 logger.debug("I am sent to standard out.")
