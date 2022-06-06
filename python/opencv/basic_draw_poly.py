833aa71b90283f04d83ec74daad0f2003c74c43b
alogirhtmlover <yubo.upt@gmail.com>
2022-06-06 10:53:36 +0800

    update references

--- python/opencv/basic_draw_poly.py
+++ python/opencv/basic_draw_poly.py
@@ -1,4 +1,4 @@
 # -*-coding:utf-8 -*-
 
-# refer to https://segmentfault.com/a/1190000015585635
+# refer to https://segmentfault.com/a/1190000015585635 and
 # cv2.line(), cv2.circle() , cv2.rectangle(), cv2.ellipse(), cv2.putText()
