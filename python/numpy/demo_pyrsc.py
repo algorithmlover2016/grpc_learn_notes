2e617b76536e9e8b1d559d55149a421c35868351
alogirhtmlover <yubo.upt@gmail.com>
2022-07-26 15:42:07 +0800

    modify threshold value

--- python/numpy/demo_pyrsc.py
+++ python/numpy/demo_pyrsc.py
@@ -5,9 +5,7 @@ import pyransac3d as pyrsc
 import cv2
 from pathlib import Path
 import pdb
-
-from torch import int8
-pdb.set_trace()
+# pdb.set_trace()
 
 for filename in ["./points_222-depth_k.npz", "./points_333-depth_k.npz"]:
     data = np.load(filename)
@@ -19,9 +17,9 @@ for filename in ["./points_222-depth_k.npz", "./points_333-depth_k.npz"]:
     print(all.shape, index.shape, points.shape)
     plane = pyrsc.Plane()
     # best_inliers = np.arange(all_points_len)
-    for i in range(2):
-        thresh = np.sqrt(sum(np.sqrt(sum((points - points.mean())**2)/(points.shape[0] - 1)) ** 2)) / 16
-        best_eq, best_inliers = plane.fit(points, max(thresh, 1), maxIteration = 10000)
+    for i in range(5):
+        thresh = np.sqrt(sum(np.sqrt(sum((points - points.mean())**2)/(points.shape[0] - 1)) ** 2)) / 53
+        best_eq, best_inliers = plane.fit(points, max(thresh, 1), maxIteration = 80000)
         print(best_eq, best_inliers.shape, "%.4f" % thresh, "{:.4f}".format(best_inliers.shape[0]/all_points_len * 100))
         points = points[best_inliers]
     print(f"final {filename}: {best_eq}, {best_inliers.shape}, {best_inliers.shape[0]/all_points_len * 100:.4f}")
