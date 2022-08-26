1481c432d48ad8e60e3b3e7e0b1d553badaa18a8
alogirhtmlover <yubo.upt@gmail.com>
2022-08-26 17:03:24 +0800

    add torch.cat demo

--- python/numpy/demo_random.py
+++ python/numpy/demo_random.py
@@ -1,3 +1,10 @@
 #-*- coding:utf-8 -*-
+import torch
 
 # https://zhuanlan.zhihu.com/p/162144706
+index = torch.tensor([range(100)])
+y = (index // 40).float()
+x = (index % 40).float()
+print(y, x)
+xy = torch.cat([y[..., None], x[..., None]], dim = -1)
+print(xy)
