b8375e508f09c36fc39f864eb84a528780d6f89c
alogirhtmlover <yubo.upt@gmail.com>
2022-10-17 11:57:03 +0800

    add input line to integer (float)

---
+++ cplusplus/iostream/input_line.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::string fileName("./input.txt");
+
+    std::ifstream inputFile(fileName);
+    if (!inputFile.is_open()) {
+        std::cout << "open [" << fileName << "] encounter error" << "\n";
+        return -1;
+    }
+    std::string inStrLine;
+    while (std::getline(inputFile, inStrLine)) {
+        if (inStrLine.empty()) {
+            continue;
+        }
+        std::cout << "input: " << inStrLine << "\n";
+        std::stringstream ss(inStrLine);
+        std::string str;
+        while (ss >> str) {
+            try {
+                if (":" == str) {
+                    std::cout << str;
+
+                } else {
+                    int num = std::stof(str);
+                    std::cout << num << ",\t";
+                }
+            } catch (std::invalid_argument const & ex) {
+                std::cout << "\nTo float [" << str << "] encounter " << ex.what() << "\n";
+            } catch (std::out_of_range const & ex) {
+                continue;
+            }
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+// g++ -std=c++11 ./input_line.cpp -o bin_output
