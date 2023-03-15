136aa83a91deee5c0114c120de2d9e0247e56e49
alogirhtmlover <yubo.upt@gmail.com>
2023-03-15 15:50:05 +0800

    add torch cuda

--- python/numpy/demo_random.py
+++ python/numpy/demo_random.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 import torch
+torch.cuda.empty_cache()
+print('memory_allocated', torch.cuda.memory_allocated() / 1e9, 'memory_cached', torch.cuda.memory_cached() / 1e9)
 
 # https://zhuanlan.zhihu.com/p/162144706
 index = torch.tensor([range(100)]) # torch.gather(tensor, dim, index)
